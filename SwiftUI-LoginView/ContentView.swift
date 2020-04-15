b7061a47255dde6566f2830e9f1fa224f8faf95e
Maksim <malyshev.maksim99@gmail.com>
2020-04-15 17:19:49 +0300

    Init

---
+++ SwiftUI-LoginView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-LoginView
+//
+//  Created by Максим on 15.04.2020.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
