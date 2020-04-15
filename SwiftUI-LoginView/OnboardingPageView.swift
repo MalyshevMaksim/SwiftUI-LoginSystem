7acc2c2920d32cadb16471663106060c656e589e
Maksim <malyshev.maksim99@gmail.com>
2020-04-15 21:11:37 +0300

    Adding a wrapper pageViewController for swiftUI

---
+++ SwiftUI-LoginView/OnboardingPageView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingPageView.swift
+//  SwiftUI-LoginView
+//
+//  Created by Максим on 15.04.2020.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingPageView()
+    }
+}
---
+++ SwiftUI-LoginView/OnboardingView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingView.swift
+//  SwiftUI-LoginView
+//
+//  Created by Максим on 15.04.2020.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
