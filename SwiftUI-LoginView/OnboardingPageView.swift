013bcfdfde45a99f7e04ac8a70695009d9981762
Maksim <malyshev.maksim99@gmail.com>
2020-04-16 00:00:33 +0300

    Add onboarding view

--- SwiftUI-LoginView/OnboardingPageView.swift
+++ SwiftUI-LoginView/OnboardingPageView.swift
@@ -9,13 +9,75 @@
 import SwiftUI
 
 struct OnboardingPageView: View {
+    @State private var isApp = false
+    
+    var imageName: String
+    var title: String
+    var text: String
+    
+    var buttonGradient = LinearGradient(gradient: Gradient(colors: [Color.init(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)), Color.init(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))]), startPoint: .bottomLeading, endPoint: .topTrailing)
+    
+    var customTransition: AnyTransition {
+        let transition = AnyTransition.move(edge: .trailing)
+            .combined(with: .scale(scale: 0.1, anchor: .trailing))
+            .combined(with: .opacity)
+      return transition
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 120, height: 120, alignment: .center)
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+                .padding()
+            
+            if isApp {
+                Text(title)
+                    .bold()
+                    .frame(width: 300)
+                    .font(.system(size: 22))
+                    .animation(Animation.spring().delay(0.1)).transition(customTransition)
+            }
+            
+            if isApp {
+                Text(text)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                    .padding(10)
+                    .animation(Animation.spring().delay(0.2)).transition(customTransition)
+            }
+            
+            Button(action: { }) {
+                Text("Show more info")
+                    .bold()
+                    .font(.headline)
+            }
+            .padding(10)
+        }
+        .onAppear() {
+            self.isApp = true
+            
+        }
+        .onDisappear() {
+            self.isApp = false
+        }
+        .animation(.spring())
+        .transition(customTransition)
+    }
+    
+    init(image: String, title: String, text: String) {
+        self.imageName = image
+        self.title = title
+        self.text = text
     }
 }
 
 struct OnboardingPageView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingPageView()
+        OnboardingPageView(image: "Billie", title: "Billie Elish", text: "Американская певица и автор песен. Снискала известность в 2016 году благодаря публикации дебютного сингла «Ocean Eyes» на SoundCloud.")
     }
 }
--- SwiftUI-LoginView/OnboardingView.swift
+++ SwiftUI-LoginView/OnboardingView.swift
@@ -8,14 +8,32 @@
 
 import SwiftUI
 
-struct OnboardingView: View {
+var artists = [
+    OnboardingPageView(image: "Billie", title: "Billie Elish", text: "Американская певица и автор песен. Снискала известность в 2016 году благодаря публикации дебютного сингла «Ocean Eyes» на SoundCloud."),
+    OnboardingPageView(image: "Eminem", title: "Eminem", text: "Американская певица и автор песен. Снискала известность в 2016 году благодаря публикации дебютного сингла «Ocean Eyes» на SoundCloud."),
+    OnboardingPageView(image: "OneRepublic", title: "OneRepublic", text: "Американская певица и автор песен. Снискала известность в 2016 году благодаря публикации дебютного сингла «Ocean Eyes» на SoundCloud.")
+]
+
+struct OnboardingView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.init(#colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.9568627451, alpha: 1))
+            PageViewController(viewControllers: viewControllers)
+        }
+        .frame(width: 320, height: 450, alignment: .center)
+        .cornerRadius(25)
+        .shadow(color: Color.init(#colorLiteral(red: 0.8736248016, green: 0.8737716079, blue: 0.8736054301, alpha: 1)), radius: 20, x: 0, y: 0)
+    }
+    
+    init(_ view : [Page]) {
+        self.viewControllers = view.map { UIHostingController(rootView: $0)}
     }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(artists)
     }
 }
